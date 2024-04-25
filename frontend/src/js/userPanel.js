6d8882ed2baf7f7a1d40f4d460e8364096c3f7bc
AtiyehHoshanfar <atiyeh.hoshanfar@gmail.com>
2024-04-25 13:37:53 +0330

    remove user

--- frontend/src/js/userPanel.js
+++ frontend/src/js/userPanel.js
@@ -1,9 +1,14 @@
 const userList=document.querySelector(".user-panel__user-list")
+const modalContainer=document.querySelector(".modal-container")
+const removeUserBtns=document.querySelectorAll("#remove-user-btn")
+const cancelDeleteUserBtn=document.querySelector("#cancel-delete-user-btn")
+const backdrop=document.querySelector(".backdrop")
 
-
+// !Function
 async function getAllUser(){
    const {data}=await axios.get("http://localhost:3000/api/users") 
   await  console.log(data)
+  userList.innerHTML=""
  await data.forEach(user=>{
     userList.insertAdjacentHTML("beforeend",`   
 <li class="user-panel__user-information">
@@ -17,7 +22,7 @@ async function getAllUser(){
       <span class="user-panel__last-name">${user.lastName}</span>
     </div>
     <div class="user-panel__btns">
-      <button class="user-panel__edit-btn btn--primary">
+      <button onclick="editUserInformation('${user._id}')" class="user-panel__edit-btn btn--primary">
         Edit
       </button>
       <button onclick="showRemoveModal('${user._id}')" class="user-panel__remove-btn btn--secondary">
@@ -28,9 +33,29 @@ async function getAllUser(){
   })
 }
 function showRemoveModal(id){
-    
-    console.log(id)
+    modalContainer.classList.add("visible")
+    removeUserBtns.forEach(btn=>btn.addEventListener("click",()=>{
+        removeUser(id)
+    }))
+
+}
+function closeRemoveModal(){
+    modalContainer.classList.remove("visible")
+
+}
+function editUserInformation(){
+location.href="http://localhost:5500/frontend/src/pages/editInformation.html"
+}
+async function removeUser(id){
+    const response=await axios.delete(`http://localhost:3000/api/users/${id}`)
+    await  closeRemoveModal()
+  await  getAllUser()
+
+
 }
 
+// !Selector
 document.addEventListener("DOMContentLoaded",getAllUser)
+cancelDeleteUserBtn.addEventListener("click",closeRemoveModal)
 
+backdrop.addEventListener("click",closeRemoveModal)
